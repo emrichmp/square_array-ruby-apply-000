2af8238d473cd6280625afa116dd06c4a2120976
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-05-13 01:43:08 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
+array = [1, 2, 3, 4]
+
 def square_array(array)
   # your code here
+  counter = 0
+  array.each do |num|
+    square = num * num
+    new_array[counter] = square
+    counter += 1
+  end
 end
